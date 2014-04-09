2859bf606c0513ded188187235f5874bfea8f3ca
Peter Barron <pbarron@gilt.com>
2014-04-09 11:49:57 +0100

    NOJIRA-0 Add missing implicitConversions import
    Change-Id: I8df31e4690bb3a305ab99d0a73ea292152cd04ab

--- src/main/scala/com/gilt/timeuuid/package.scala
+++ src/main/scala/com/gilt/timeuuid/package.scala
@@ -1,6 +1,7 @@
 package com.gilt
 
 import java.util.{Date, UUID}
+import scala.language.implicitConversions
 
 package object timeuuid {
   implicit def timeUuidToDate(value: UUID): Date = new TimeUuidToDate().extract(value)
